06725fa3ee3aa63aa79db8250cf17bd04d5dfe20
Sabrina Hecht <hecht@quattro-research.com>
2015-12-04 10:29:00 +0100

    Resolve merge errors

--- src/main/java/org/helm/notation2/parser/notation/polymer/ChemEntity.java
+++ src/main/java/org/helm/notation2/parser/notation/polymer/ChemEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/org/helm/notation2/parser/notation/polymer/ChemEntity.java
 /**
  * *****************************************************************************
  * Copyright C 2015, The Pistoia Alliance
@@ -43,49 +42,3 @@ public class ChemEntity extends PolymerEntity {
   }
 
 }
-=======
-/**
- * *****************************************************************************
- * Copyright C 2015, The Pistoia Alliance
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *****************************************************************************
- */
-package parsertest.Notation.Polymer;
-
-
-/**
- * ChemEntity
- * 
- * @author hecht
- */
-public class ChemEntity extends PolymerEntity {
-
-  /**
-   * Constructs with the given string
-   * 
-   * @param str Chem ID
-   */
-  public ChemEntity(String str) {
-    super(str);
-    type = "CHEM";
-  }
-
-}
->>>>>>> 028ee9b5dc0c7557d6e1cfa25a041b95a69e9c58:src/main/java/parsertest/Notation/Polymer/ChemEntity.java
